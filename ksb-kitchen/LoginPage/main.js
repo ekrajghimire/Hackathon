cebbfbbee3baf41771c5dfddfab39ee1565dc513
Ekraj Ghimire <56933759+ekrajghimire@users.noreply.github.com>
2023-04-30 03:24:52 +0530

    Add files via upload

--- ksb-kitchen/LoginPage/main.js
+++ ksb-kitchen/LoginPage/main.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+const auth = firebase.auth()
+const database = firebase.database()
+
+function register(){
+	names = document.getElementById('name').value
+	email = document.getElementById('email').value
+	username = document.getElementById('email').value
+	password = document.getElementById('password').value
+}
